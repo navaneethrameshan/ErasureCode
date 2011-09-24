32bc7f5d6dfa7cb7efa4a75fbdc0d0023a9dcd30
Navaneeth <navaneeth.rameshan@gmail.com>
2011-09-25 04:17:50 +0530

    Logic for reconstruction and regeneration works. File joining issue.

---
+++ ReconstructVector.java
@@ -0,0 +1,9 @@
+
+public class ReconstructVector {
+	static final int PARTSPERNODE = 4;
+	static final int VECTORSIZE = 4;
+
+	public static int[][][] list = {
+			{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}}
+	};
+}
